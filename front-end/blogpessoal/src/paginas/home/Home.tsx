83195f9e684cc84f8165d36e04d21202d8438e69
João Pedro <jprocharodrigues121@gmail.com>
2021-10-25 16:45:24 -0300

    adicionando front-end React

---
+++ front-end/blogpessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://image.freepik.com/vetores-gratis/hipster-pessoas-conversando-e-usando-computadores-em-cooperacao_74855-5267.jpg" alt="imagem tela inicial" className="img"/>
+        </>
+    );
+}
+
+export default Home;
